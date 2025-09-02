b1452332fe8c45866442a5e6cd524118f71aab8d
Sonal-1610 <sonalrajwani1609@gmail.com>
2025-09-02 22:07:17 +0530

    day11

---
+++ html_codes/day11/script.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+// alert("Hello,Good Morning");
+
+// prompt("Enter a number");
+
+// console.log(prompt("Enter a number"));
+
+// confirm("Are you sure you want to referesh this page");
+// console.log(confirm("Are you sure you want to referesh this page"));
+
+var a=20;
+console.log(a);
+let b=100;
+console.log(b);
+const c=200;
+console.log(c);
+
+let $sonal=16;
+console.log($sonal);
+var first="sonal";
+console.log(first);
+
+console.log(typeof $sonal);
