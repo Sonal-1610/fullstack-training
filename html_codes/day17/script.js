86879d23e8453033ac47e110f8bf1f36bf55c898
Sonal-1610 <sonalrajwani1609@gmail.com>
2025-09-09 22:44:55 +0530

    day17

---
+++ html_codes/day17/script.js
@@ -0,0 +1,87 @@
+//math,number,date
+
+//math-sqrt,random,ceil,floor,round,pow,min,max
+
+//number-tofixed,parseInt,parsefloat,isnan,isinteger
+
+//date-getfullyear,getmonth,getday,getdate,gethours,getminutes,getseconds,gettime,now(),todatestring,totiestring
+
+//math method
+
+// let result=Math.sqrt(100);
+// console.log(result);
+
+// let result=Math.ceil(10.001);
+// console.log(result);
+// let result=Math.floor(1000+Math.random()*9000);
+// console.log(result);
+
+//number methods
+// let num=102.343545678;
+// let newnum=num.toFixed(2);
+// console.log(newnum);
+
+// let str="5";
+// let newnum=Number(str);
+// let str1="45.7987r5px67";
+// console.log(Number.parseInt(str1));
+// console.log(Number.parseFloat(str1));
+
+// let str="abc";
+// let num=Number(str);
+// console.log(num);
+
+
+// let str="son2445al689raj889wa89ni";
+// let newArr=str.split("");
+// console.log(newArr);
+// let sum=0;
+// newArr.forEach((item)=>{
+//     let newnum=Number(item);
+//     if(!Number.isNaN(newnum)){
+//         sum+=newnum;
+//     }
+// })
+
+// console.log(sum);
+
+//type conversion=explicit conversion and type coercion=implicit conversion
+
+// let f="40";
+// let num=Number(f);
+// console.log(typeof num);
+
+// let f=40;
+// let num=String(f);
+// console.log(typeof num);
+
+// let f=67;
+// let num=Boolean(f);
+// console.log( num);
+
+// let f=[1,2,3];
+
+// console.log(String(f));
+
+//type coercion
+// console.log(5+"5");
+// console.log(5+"5"-10);
+// console.log("5"+false);
+// console.log({}-{});
+// console.log([]+[]);
+// console.log({}-undefined);
+
+
+//scopes and hoisiting
+
+//block-let const,global-var,functional-var,
+
+//hoisiting
+
+console.log(a);
+let a="shubbham";
+console.log(a);
+
+
+
+
