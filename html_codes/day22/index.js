98b1f9168b7e934742f4bb6e025596a239c2da6e
Sonal-1610 <sonalrajwani1609@gmail.com>
2025-09-21 22:20:35 +0530

    html forms practise

--- html_codes/day22/index.js
+++ html_codes/day22/index.js
@@ -1,1 +1,12 @@
 //git and github
+//import exports
+//1  cjs-common js 
+//2  mjs-module js
+
+function sum(){
+    console.log("sum is called");
+}
+
+// const {sum:add,sub}=require("./mathoperation");
+
+
