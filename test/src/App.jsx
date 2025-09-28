46f9e789bcbb3417368c6ac2a7fa5d80a9e985c4
Sonal-1610 <sonalrajwani1609@gmail.com>
2025-09-28 21:01:56 +0530

    uneccesary

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,50 +1,14 @@
 import { RouterProvider } from "react-router-dom";
-// import Card from "./Card";
+import Card from "./Card";
 
-// import Effect from "./Effect";
+import Effect from "./Effect";
 
-// import Login from "./Login";
-// import Navbar from "./Navbar";
+import Login from "./Login";
+import Navbar from "./Navbar";
 import Router from "./Router";
 
 function App() {
-  // const [count, setCount] = useState(0)
   
-  // let pName="Samsung S24 Ultra";
-  // return (
-
-  //   <div className='container'>
-      
-  //       <Card title={pName} description="This is s series of samsung" image="https://tse3.mm.bing.net/th/id/OIP.t5i7y3B3ozp4L5F_trymRQHaId?pid=Api&P=0&h=180" />
-
-  //        <Card title={pName} description="This is s series of vivo" image="https://tse4.mm.bing.net/th/id/OIP.Pv413ZU7i-oYEtElc8dgNwHaOr?pid=Api&P=0&h=180" />
-  //        <Card />
-  //        <Card />
-
-  //   </div>
-    // <>
-    //   <div>
-    //     <a href="https://vite.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
-  // )
   return(
     <div>
     <RouterProvider router={Router} />
