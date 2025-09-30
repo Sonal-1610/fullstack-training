8a10a4881e7c188c9239346f03a19dfb5e685aae
Sonal-1610 <sonalrajwani1609@gmail.com>
2025-09-30 19:13:28 +0530

    no needed

--- test/src/Router.jsx
+++ test/src/Router.jsx
@@ -6,29 +6,6 @@ import { createBrowserRouter } from 'react-router-dom'
  import Home from './Home'
  import About from './About'
 
-
-
-
-//  import {BrowserRouter,Route,Routes} from 'react-router-dom'
-// const Router = () => {
-//   return (
-//     <div>
-//         <BrowserRouter>
-//         <Routes>
-//             <Route path='/dashboard' element=
-//             {<Dashboard /> }/>
-
-//             <Route path='/login' element={<Login />} />
-//              </Routes>
-//         </BrowserRouter>
-       
-        
-//     </div>
-    
-//   )
-// }
-
-
 const Router =createBrowserRouter([
   {
     element: <Parent/> ,
